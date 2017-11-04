b5d4ec1c7eb3037b23429d569d7ea9954f1ea7b0
TJ <tjchase34@gmail.com>
2017-11-04 14:00:37 -0400

    Added get_distance function

--- inverse-roomba.cpp
+++ inverse-roomba.cpp
@@ -1,10 +1,34 @@
 #include <Arduino.h>
 #include "roomba.h"
 
-void setup() {
-	
-}
+int trigPin = 3;
+int echoPin = 2;
+int distance;
+long duration;
 
+int get_distance() {
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+
+	duration = pulseIn(echoPin, HIGH);
+
+	distance = duration*0.034/2;
+
+	Serial.print("Distance: ");
+	Serial.println(distance);
+
+	return distance;
+}
+void setup() {
+	pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+	pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+	Serial.begin(9600); // Starts the serial communication
+}
 void loop() {
+
 	
+
 }
