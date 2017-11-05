efa6ce408d17fb2d9bb361831692fa359164f3a9
TJ <tjchase34@gmail.com>
2017-11-05 14:07:42 -0500

    Removed include

--- inverse-roomba.cpp
+++ inverse-roomba.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include "roomba.h"
 
 void step_motor() {
   digitalWrite(11, LOW);
@@ -132,10 +131,10 @@ void setup() {
     pinMode(7, OUTPUT);
     pinMode(8, OUTPUT);
     pinMode(9, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(13, OUTPUT);
+    // pinMode(10, OUTPUT);
+    // pinMode(11, OUTPUT);
+    // pinMode(12, OUTPUT);
+    // pinMode(13, OUTPUT);
 }
 void loop() {
 
